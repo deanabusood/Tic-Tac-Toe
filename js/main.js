b9f46873a37056ae1d6d460cec467230ef7df6b7
dean <dnabusood@gmail.com>
2023-02-12 14:42:48 -0500

    Add handleTurn function and eventlistener to the board

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 let board;
+let turn = "X";
 /*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll("#board div"));
 /*----- event listeners -----*/
+document.getElementById("board").addEventListener("click", handleTurn);
 /*----- functions -----*/
 function init() {
   board = ["", "", "", "", "", "", "", "", ""]; //3x3 board
@@ -13,7 +16,18 @@ function init() {
 function render() {
   board.forEach(function (mark, index) {
     console.log(mark, index);
+
+    squares[index].textContent = mark;
   });
 }
+
+function handleTurn(event) {
+  let idx = squares.findIndex(function (square) {
+    return square === event.target;
+  });
+  board[idx] = turn;
+  //   console.log(board);
+}
+
 // call init
 init();
