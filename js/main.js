f5545a8bbb6b845361b90723a3ccf9b7058ddd27
dean <dnabusood@gmail.com>
2023-02-12 14:37:25 -0500

    Create functions to initialize and render the board

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-console.log(
-  "Your JS is linked up. Be the person you needed when you were little."
-);
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+let board;
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+function init() {
+  board = ["", "", "", "", "", "", "", "", ""]; //3x3 board
+
+  render();
+}
+
+function render() {
+  board.forEach(function (mark, index) {
+    console.log(mark, index);
+  });
+}
+// call init
+init();
